feaa0ab24cd544b2d64954b3a0696ed534d11256
Christian Bosshard <bosshardchr@gmail.com>
2019-05-02 20:02:53 +0200

    Add junit 5 and assertJ.

---
+++ src/test/java/ch/cbossi/playground/vertx/PlaygroundControllerTest.java
@@ -0,0 +1,13 @@
+package ch.cbossi.playground.vertx;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlaygroundControllerTest {
+
+  @Test
+  void testGreeting() {
+    assertThat(true).isTrue();
+  }
+}
