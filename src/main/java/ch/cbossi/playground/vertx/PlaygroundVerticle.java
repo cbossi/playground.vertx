5e5f7c3b338f5a0e6e5b6d80d1694c8f9821b538
Christian Bosshard <christian.bosshard@ergon.ch>
2019-05-02 17:31:45 +0200

    Create simple HTTP server.

--- src/main/java/ch/cbossi/playground/vertx/PlaygroundVerticle.java
+++ src/main/java/ch/cbossi/playground/vertx/PlaygroundVerticle.java
@@ -1,11 +1,35 @@
 package ch.cbossi.playground.vertx;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
 
 class PlaygroundVerticle extends AbstractVerticle {
 
+  private final Vertx vertx;
+
+  private HttpServer httpServer;
+
+  PlaygroundVerticle(Vertx vertx) {
+    this.vertx = vertx;
+  }
+
   @Override
-  public void start() {
-    System.out.println("Hello Vert.x");
+  public void start(Future<Void> future) {
+    httpServer = vertx.createHttpServer()
+        .requestHandler(request -> request.response().end("Hello Vert.x"))
+        .listen(8080, httpServer -> {
+          if (httpServer.succeeded()) {
+            future.complete();
+          } else {
+            future.fail(httpServer.cause());
+          }
+        });
+  }
+
+  @Override
+  public void stop() {
+    httpServer.close();
   }
 }
--- src/main/java/ch/cbossi/playground/vertx/VertxPlaygroundApplication.java
+++ src/main/java/ch/cbossi/playground/vertx/VertxPlaygroundApplication.java
@@ -5,8 +5,9 @@ import io.vertx.core.Vertx;
 public class VertxPlaygroundApplication {
 
   public static void main(String[] args) {
+    System.setProperty("vertx.disableDnsResolver", "true"); // see https://github.com/eclipse-vertx/vert.x/issues/2369#issuecomment-439722563
     Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new PlaygroundVerticle());
+    vertx.deployVerticle(new PlaygroundVerticle(vertx));
   }
 
 }
