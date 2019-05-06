3799c2411db8237d12fb388baddb6c374205ace7
Christian Bosshard <christian.bosshard@ergon.ch>
2019-05-06 13:47:02 +0200

    Create and configure separate source set for integration tests.

---
+++ src/inttest/java/ch/cbossi/playground/vertx/IntegrationTest.java
@@ -0,0 +1,47 @@
+package ch.cbossi.playground.vertx;
+
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Map;
+
+import static ch.cbossi.playground.vertx.PlaygroundController.NAME_PARAM;
+import static ch.cbossi.playground.vertx.Uris.createUri;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(VertxExtension.class)
+public class IntegrationTest {
+
+  private static WebClient webClient;
+
+  @BeforeEach
+  void deployVerticle(io.vertx.core.Vertx vertx, VertxTestContext testContext) {
+    ApplicationInitializer.of(vertx)
+        .withCompletionHandler(testContext.completing())
+        .withOverride(new MockModule())
+        .deploy();
+    webClient = createWebClient(vertx);
+  }
+
+  private static WebClient createWebClient(Vertx vertx) {
+    WebClientOptions options = new WebClientOptions().setDefaultPort(8080).setDefaultHost("localhost");
+    return WebClient.create(vertx, options);
+  }
+
+  @Test
+  void testGreeting(VertxTestContext testContext) {
+    webClient.get(createUri(PlaygroundController.GREETING_URL, Map.of(NAME_PARAM, "max")))
+        .send(testContext.succeeding(response -> testContext.verify(() -> {
+          PlaygroundController.GreetingTO greeting = response.bodyAsJson(PlaygroundController.GreetingTO.class);
+          assertThat(greeting.getGreeting()).isEqualTo("Hello mocked Max Muster");
+          testContext.completeNow();
+        })));
+  }
+}
